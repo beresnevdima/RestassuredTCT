fba51a963599f488e6a6bace4eedf7bb2c021aaa
dmytroberesniev <dimadrossel@gmail.com>
2023-01-10 21:52:49 +0200

    selectDB

--- src/main/java/org/example/RestassuredTCT/DB/DragonDBConnection.java
+++ src/main/java/org/example/RestassuredTCT/DB/DragonDBConnection.java
@@ -4,22 +4,12 @@ import java.io.IOException;
 import java.sql.*;
 
 
-public class DragonSandboxConnection {
-    public static void main(String[] args) throws IOException {
+public class DragonDBConnection {
+    public static void main(String[] args) throws IOException, SQLException, ClassNotFoundException {
         System.getProperties().load(ClassLoader.getSystemResourceAsStream("application.properties"));
         String jdbcUrl = System.getProperty("dragon.sandbox.jdbcUrl");
         String username = System.getProperty("dragon.sandbox.username");
         String password = System.getProperty("dragon.sandbox.password");
-        Connection connection;
-
-        try {
-            connection = DriverManager.getConnection(jdbcUrl, username, password);
-            if (!connection.isClosed()) {
-                System.out.println("We are connected!");
-            }
-        } catch (SQLException e) {
-            System.out.println("there is no connection... Exception!");
-        }
 
         try (Connection conn = DriverManager.getConnection(jdbcUrl, username, password);
              Statement stmt = conn.createStatement()) {
@@ -30,7 +20,6 @@ public class DragonSandboxConnection {
                     String name = rs.getString("name");
                     System.out.println(name);
                 }
-
             } catch (SQLException e ) {
                 throw new Error("Problem", e);
             }
