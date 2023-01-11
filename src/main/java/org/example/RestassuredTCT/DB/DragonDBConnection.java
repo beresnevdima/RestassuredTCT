4d80408633112e296a5f0787021d09d3be1dac5f
dmytroberesniev <dimadrossel@gmail.com>
2023-01-11 13:23:57 +0200

    equals(name)

--- src/main/java/org/example/RestassuredTCT/DB/DragonDBConnection.java
+++ src/main/java/org/example/RestassuredTCT/DB/DragonDBConnection.java
@@ -3,7 +3,6 @@ package org.example.RestassuredTCT.DB;
 import java.io.IOException;
 import java.sql.*;
 
-
 public class DragonDBConnection {
     public static void main(String[] args) throws IOException, SQLException, ClassNotFoundException {
         System.getProperties().load(ClassLoader.getSystemResourceAsStream("application.properties"));
@@ -15,10 +14,17 @@ public class DragonDBConnection {
              Statement stmt = conn.createStatement()) {
 
             try {
-                ResultSet rs = stmt.executeQuery("select name from un62.process_state");
+                ResultSet rs = stmt.executeQuery("SELECT state FROM un62.interest_charges_v2 WHERE acc='21100200025461' AND date_calc='2022-09-01'");
                 while (rs.next()) {
-                    String name = rs.getString("name");
+                    String name = rs.getString("state");
                     System.out.println(name);
+
+                    if ("G".equals(name)) {
+                        System.out.println("Есть совпадение");
+                    }
+                        else {
+                        System.out.println("Ошибка");
+                    }
                 }
             } catch (SQLException e ) {
                 throw new Error("Problem", e);
