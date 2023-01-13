9a3b2dc1814de5a1cf0d06148cd096f9d41f8f54
dmytroberesniev <dimadrossel@gmail.com>
2023-01-13 15:59:32 +0200

    Refactor

--- src/main/java/org/example/RestassuredTCT/DB/DragonDBConnection.java
+++ src/main/java/org/example/RestassuredTCT/DB/DragonDBConnection.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.sql.*;
 
 public class DragonDBConnection {
-    public static void main(String[] args) throws IOException, SQLException, ClassNotFoundException {
+    public static void main(String[] args) throws IOException {
         System.getProperties().load(ClassLoader.getSystemResourceAsStream("application.properties"));
         String jdbcUrl = System.getProperty("dragon.sandbox.jdbcUrl");
         String username = System.getProperty("dragon.sandbox.username");
@@ -15,7 +15,7 @@ public class DragonDBConnection {
         ResultSet rs =null;
             try {
 
-                int i = stmt.executeUpdate("UPDATE un62.interest_charges_v2 SET interest=0.01, accum=0.11, state='G', amount=100.00, debit=0.00, credit=0.00, is_double=NULL, is_end_of_grace=NULL, rate=37.50, client_id=500981 WHERE acc='21100200025461' AND date_calc='2022-09-01'");
+                int i = stmt.executeUpdate("UPDATE un62.interest_charges_v2 set state='G' WHERE acc='21100200025461' AND date_calc='2022-09-01'");
                 System.out.println(i);
 
                 rs = stmt.executeQuery("SELECT state FROM un62.interest_charges_v2 WHERE acc='21100200025461' AND date_calc='2022-09-01'");
