6151d3922a633f226de850e608fdf6ce8ea559da
dmytroberesniev <dimadrossel@gmail.com>
2023-01-16 17:25:05 +0200

    doSelectInteger

--- src/main/java/org/example/RestassuredTCT/DB/Example/SqlConnection.java
+++ src/main/java/org/example/RestassuredTCT/DB/Example/SqlConnection.java
@@ -31,15 +31,64 @@ public class SqlConnection {
         }
         return DriverManager.getConnection(jdbcUrl, username, password);
     }
-@Test
-    public void doUpdate (String sqlSet){
+
+
+    public void doUpdate(String sqlSet) {
         try (Connection connect = DriverManager.getConnection(jdbcUrl, username, password);
-             Statement stmt = connect.createStatement()){
+             Statement stmt = connect.createStatement()) {
             stmt.executeUpdate(sqlSet);
-        }
-        catch (SQLException e) {
+        } catch (SQLException e) {
             throw new Error("Problem", e);
         }
     }
 
+    public void doSelectString(String sqlSelectString, String columnLabelString, String valueString) throws SQLException {
+        ResultSet res = null;
+        try (Connection connect = DriverManager.getConnection(jdbcUrl, username, password);
+             Statement stmt = connect.createStatement()) {
+            res = stmt.executeQuery(sqlSelectString);
+
+            while (res.next()) {
+                String name = res.getString(columnLabelString);
+                System.out.println(name);
+
+                if (valueString.equals(name)) {
+                    System.out.println("Есть совпадение");
+                } else {
+                    System.out.println("Ошибка");
+                }
+            }
+        } catch (SQLException e) {
+            throw new Error("Problem", e);
+        } finally {
+            if (res != null) {
+                res.close();
+            }
+        }
+    }
+
+    public void doSelectInteger(String sqlSelectInteger, String columnLabelInteger, Long valueInteger) throws SQLException {
+        ResultSet res = null;
+        try (Connection connect = DriverManager.getConnection(jdbcUrl, username, password);
+             Statement stmt = connect.createStatement()) {
+            res = stmt.executeQuery(sqlSelectInteger);
+
+            while (res.next()) {
+                Long name = res.getLong(columnLabelInteger);
+                System.out.println(name);
+
+                if (valueInteger.equals(name)) {
+                    System.out.println("Есть совпадение");
+                } else {
+                    System.out.println("Ошибка");
+                }
+            }
+        } catch (SQLException e) {
+            throw new Error("Problem", e);
+        } finally {
+            if (res != null) {
+                res.close();
+            }
+        }
+    }
 }
--- src/test/java/org/example/RestassuredTCT/CreditDetails/Script1/Case2.java
+++ src/test/java/org/example/RestassuredTCT/CreditDetails/Script1/Case2.java
@@ -6,19 +6,28 @@ package org.example.RestassuredTCT.CreditDetails.Script1;
 
 import org.example.RestassuredTCT.CreditDetails.CreditDetailsTemplate;
 import org.example.RestassuredTCT.DB.Example.SqlConnection;
+import org.example.RestassuredTCT.DTO.Response.ResponseCreditDetails;
 import org.junit.Test;
 import java.io.IOException;
 import java.sql.SQLException;
+import static org.hamcrest.Matchers.equalTo;
 
 public class Case2 {
 
     String sqlSet = "delete FROM un62.debt_operation WHERE acc= '21100200006917';";
+    String sqlSelectString = "SELECT state FROM un62.interest_charges_v2 WHERE acc='21100200025461' AND date_calc='2022-09-01'";
+    String columnLabelString = "state";
+    String valueString = "G";
+    String sqlSelectInteger = "select acc from un62.interest_charges_v2 where date_calc = '2022-09-01'";
+    String columnLabelInteger = "acc";
+    Long valueInteger = 21100200025461L;
+
     long uid = 2020005024L;
     int clientId = 500341;
 
 
     SqlConnection sqlConnection = new SqlConnection();
-//    CreditDetailsTemplate creditDetailsTemplate = new CreditDetailsTemplate();
+    CreditDetailsTemplate creditDetailsTemplate = new CreditDetailsTemplate();
 
     public Case2() throws IOException {}
 
@@ -26,16 +35,18 @@ public class Case2 {
     public void Case2test () throws IOException, SQLException {
         sqlConnection.getConnection();
         sqlConnection.doUpdate(sqlSet);
-//        creditDetailsTemplate.CreditDetails(clientId, uid)
-//                .statusCode(200)
-//                .body("result.script", equalTo("1"))
-//                .body("result.totalSum", equalTo(45.00F))
-//                .body("result.paymentDue", equalTo("2023-02-28"))
-//                .body("result.fullRepay", equalTo(45.00F))
-//                .body("result.paymentState", equalTo("noMinPaym"))
-//                .body("result.contractState", equalTo("ACTIVE"))
-//                .body("result.graceStart", equalTo("2023-01-02T00:00:00.000Z"))
-//                .body("result.graceEnd", equalTo("2023-02-28T00:00:00.000Z"))
-//                .extract().as(ResponseCreditDetails.class);
+        sqlConnection.doSelectString(sqlSelectString, columnLabelString, valueString);
+        sqlConnection.doSelectInteger(sqlSelectInteger, columnLabelInteger, valueInteger);
+        creditDetailsTemplate.CreditDetails(clientId, uid)
+                .statusCode(200)
+                .body("result.script", equalTo("1"))
+                .body("result.totalSum", equalTo(45.00F))
+                .body("result.paymentDue", equalTo("2023-02-28"))
+                .body("result.fullRepay", equalTo(45.00F))
+                .body("result.paymentState", equalTo("noMinPaym"))
+                .body("result.contractState", equalTo("ACTIVE"))
+                .body("result.graceStart", equalTo("2023-01-02T00:00:00.000Z"))
+                .body("result.graceEnd", equalTo("2023-02-28T00:00:00.000Z"))
+                .extract().as(ResponseCreditDetails.class);
     }
 }
