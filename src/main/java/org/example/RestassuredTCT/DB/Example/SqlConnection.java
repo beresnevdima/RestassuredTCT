9f203cbe52d1d8bc2e5d2cd8431ceb889eee32f1
dmytroberesniev <dimadrossel@gmail.com>
2023-01-16 14:47:01 +0200

    Refactor

--- src/main/java/org/example/RestassuredTCT/DB/Example/SqlConnection.java
+++ src/main/java/org/example/RestassuredTCT/DB/Example/SqlConnection.java
@@ -4,19 +4,8 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 public class SqlConnection {
-
-    private String sqlSet;
-    private String sqlGet;
-
-    public SqlConnection(String sqlSet) {
-        this.sqlSet = sqlSet;
-
-    }
-
-
 
     public Connection getConnection() throws SQLException, IOException {
         System.getProperties().load(ClassLoader.getSystemResourceAsStream("application.properties"));
@@ -24,17 +13,15 @@ public class SqlConnection {
         String username = System.getProperty("dragon.sandbox.username");
         String password = System.getProperty("dragon.sandbox.password");
 
-        try (Connection connection = DriverManager.getConnection(jdbcUrl, username, password);
-             Statement stmt = connection.createStatement()) {
-
-        } catch (SQLException e ) {
+        try (Connection connection = DriverManager.getConnection(jdbcUrl, username, password)) {
+        } catch (SQLException e) {
             throw new Error("Problem", e);
         }
-
         return DriverManager.getConnection(jdbcUrl, username, password);
-
-
     }
+}
+
+
 //
 //    public void doinsert() throws SQLException{
 //        try(Connection connection = getConnection()){
